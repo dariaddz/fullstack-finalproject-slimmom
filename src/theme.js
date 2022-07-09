15a5e9ea79dd6e5c5c0755b53004cb84b6ba7dbf
dariaddz <dariadz@gmail.com>
2022-07-09 20:15:02 +0300

    mui theme

---
+++ src/theme.js
@@ -0,0 +1,32 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#9B9FAA',
+      dark: '#212121',
+    },
+    secondary: {
+      main: '#FC842D',
+    },
+    background: {
+      main: '#FFFFFF',
+      darker: '#F0F1F3',
+      dark: '#264061',
+      modal: 'rgba(33, 33, 33, 0.12)',
+    },
+  },
+  typography: {
+    fontSize: 14,
+    fontWeight: 400,
+    lineHeight: 1.2,
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 320,
+      md: 768,
+      lg: 1280,
+    },
+  },
+});
