080e5fa568bc5596e373def60d960f30a46d9cef
Oleksandr Eirikh <o.eyrikh@gmail.com>
2022-07-12 18:14:01 +0300

    added logic

--- src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
+++ src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
@@ -1,22 +1,41 @@
-import React from "react";
-import { Close } from "@mui/icons-material";
-import s from "./DiaryProductsListItem.module.css";
+import React from 'react';
+import s from './DiaryProductsListItem.module.css';
+import { Close } from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteProduct } from '../../redux/day/day_operation';
+import { date, dateId } from '../../redux/day/day_selector';
 
-function DiaryProductsListItem({ product: { _id, title, weight, kcal } }) {
-  const handleClick = () => {};
+const DiaryProductsListItem = ({ product: { _id, title, weight, kcal } }) => {
+  const dispatch = useDispatch();
+  const dayId = useSelector(dateId);
+  const currentDate = useSelector(date);
+
+  const today = new Date(
+    new Date().getTime() - new Date().getTimezoneOffset() * 60000
+  )
+    .toISOString()
+    .split('T')[0];
+
+  const handleClick = async () => {
+    if (currentDate === today) {
+      dispatch(deleteProduct(dayId, _id));
+    }
+  };
+
+  const disabledIcon = currentDate === today ? null : s.disabled;
 
   return (
     <>
       <li className={s.item}>
         <div className={s.name}>{title}</div>
-        <div className={s.weight}>{weight} </div>
+        <div className={s.weight}>{weight}</div>
         <div className={s.calories}>
           {kcal} <span>ккал</span>
         </div>
-        <Close className={s.icon} onClick={handleClick} />
+        <Close className={`${s.icon} ${disabledIcon}`} onClick={handleClick} />
       </li>
     </>
   );
-}
+};
 
 export default DiaryProductsListItem;
